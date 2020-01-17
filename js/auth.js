d2f9f52a102494007dade45ff34b1fb124be2cfe
faris-mohamed10 <farizmohamed1999@gmail.com>
2020-01-17 21:01:03 +0530

    authentication backend completed

--- js/auth.js
+++ js/auth.js
@@ -1,8 +1,10 @@
 /* Handles the sign up button press. */
-function handleSignUp() {
+function handleSignUpWithEmail() {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var cpassword = document.getElementById("cpassword").value;
+
+    //form validation
     if (email.length < 4) {
       alert("Please enter an email address.");
       return;
@@ -17,10 +19,7 @@ function handleSignUp() {
       return;
     }
     // Create user with email and pass.
-    // [START createwithemail]
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
+    firebase.auth().createUserWithEmailAndPassword(email, password)
       .then(function(result) {
           //add displayName to the user
           result.user.updateProfile({
@@ -33,26 +32,61 @@ function handleSignUp() {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        // [START_EXCLUDE]
         if (errorCode == "auth/weak-password") {
           alert("The password is too weak.");
         } else {
           alert(errorMessage);
         }
         console.log(error);
-        // [END_EXCLUDE]
       });
-    // [END createwithemail]
-  }
+}
+function handleSignUpWithGoogle(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+    //set google as provider
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+function handleLoginWithEmail(){
+  var email = document.getElementById("login-email").value;
+  var password = document.getElementById("login-password").value;
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    if (errorCode === 'auth/wrong-password') {
+      alert('Wrong password.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+  });
+}
   
-  window.onload = function() {
-      document.getElementById('signup').addEventListener('click', handleSignUp, false);
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          console.log(user);
-        }
-      });
-    };
+  // window.onload = function() {
+  //     document.getElementById('signup').addEventListener('click', this.handleSignUpWithEmail, false);
+  //     document.getElementById('google').addEventListener('click', this.handleSignUpWithGoogle, false);
+  //     document.getElementById('login').addEventListener('click', this.handleLoginWithEmail, false);
+  //     firebase.auth().onAuthStateChanged(function(user) {
+  //       if (user) {
+  //         console.log(user);
+  //       }
+  //     });
+  //   }
   
   
   
